3671f0322fac8ae2dabe0a81433cfcbdfd578613
Aleksey Komarov <q4arus@ya.ru>
2026-07-26 10:44:58 +0300

    PhysX: remove class UserStream

--- src/Rock3dEngine/header/px/Stream.h
+++ src/Rock3dEngine/header/px/Stream.h
@@ -4,29 +4,6 @@
 #include "NxStream.h"
 
 
-class UserStream : public NxStream
-	{
-	public:
-								UserStream(const char* filename, bool load);
-	virtual						~UserStream();
-
-	virtual		NxU8			readByte()								const;
-	virtual		NxU16			readWord()								const;
-	virtual		NxU32			readDword()								const;
-	virtual		float			readFloat()								const;
-	virtual		double			readDouble()							const;
-	virtual		void			readBuffer(void* buffer, NxU32 size)	const;
-
-	virtual		NxStream&		storeByte(NxU8 b);
-	virtual		NxStream&		storeWord(NxU16 w);
-	virtual		NxStream&		storeDword(NxU32 d);
-	virtual		NxStream&		storeFloat(NxReal f);
-	virtual		NxStream&		storeDouble(NxF64 f);
-	virtual		NxStream&		storeBuffer(const void* buffer, NxU32 size);
-
-	FILE*	fp;
-	};
-
 class MemoryWriteBuffer : public NxStream
 	{
 	public:
